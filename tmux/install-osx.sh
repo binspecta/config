de977e1ae7f978bb3a04305d55986266464f8ef0
binspecta <binspecta@gmail.com>
2017-11-05 00:37:13 +0900

    update

--- tmux/install-osx.sh
+++ tmux/install-osx.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+brew install python
+ln -s /usr/local/bin/python2 /usr/local/bin/python
+ln -s /usr/local/bin/pip2 /usr/local/bin/pip
+pip install powerline-status
+
+brew install reattach-to-user-namespace
+
 cp tmux.shared.conf ~/.tmux.shared.conf
 cp tmux.conf.osx ~/.tmux.conf
 
