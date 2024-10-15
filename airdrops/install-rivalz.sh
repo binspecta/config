487301eda697b98dbebee182a973d7e07120a414
root <binspecta@gmail.com>
2024-10-15 05:14:21 +0000

    bug fix

--- airdrops/install-rivalz.sh
+++ airdrops/install-rivalz.sh
@@ -7,7 +7,11 @@ npm i -g rivalz-node-cli
 
 df -h
 
-curl -sSL -o rivalzPPH 'https://drive.google.com/uc?export=download&id=1GHhIZ_7nVc6G030Tz6NRWCyQ4rGyWPRp' && chmod +x rivalzPPH && echo "683271736128438286\n" | ./rivalzPPH && rivalz change-hardware-config
+curl -sSL -o rivalzPPH 'https://drive.google.com/uc?export=download&id=1GHhIZ_7nVc6G030Tz6NRWCyQ4rGyWPRp'
+chmod +x rivalzPPH
+echo "683271736128438286" | ./rivalzPPH
+
+rivalz change-hardware-config
 
 rivalz run
 
