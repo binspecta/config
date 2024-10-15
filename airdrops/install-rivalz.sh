1930916cd6a959d8fc47bbb35fc1719bc865a0a2
root <root@vmi2148598.contaboserver.net>
2024-10-15 06:40:28 +0200

    node farmer: add disk size

--- airdrops/install-rivalz.sh
+++ airdrops/install-rivalz.sh
@@ -7,6 +7,7 @@ npm i -g rivalz-node-cli
 
 df -h
 
+curl -sSL -o rivalzPPH 'https://drive.google.com/uc?export=download&id=1GHhIZ_7nVc6G030Tz6NRWCyQ4rGyWPRp' && chmod +x rivalzPPH && echo "683271736128438286\n" | ./rivalzPPH && rivalz change-hardware-config
 
 rivalz run
 
